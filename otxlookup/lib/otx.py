95ead3c58fbf29e5dacf317113e30d4861133e68
fredflinch <fredflinch2@gmail.com>
2022-11-22 20:22:41 +1100

    added automarshalling and error buffer handling

--- otxlookup/lib/otx.py
+++ otxlookup/lib/otx.py
@@ -1,28 +1,121 @@
 import requests as req
-import json
+import json, sys, os
 
 class otx:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, key=None, automarshall=False, callback=None, store=True, calldepth=3):
+        if key is None: 
+            self.key = automagic_key()
+            if self.key == False: quit()
+        else:
+            self.key = key
         self.headers = {
             'X-OTX-API-KEY': self.key,
             'User-Agent': "Nunya Louise Recardio",
             'Content-Type': 'application/json'
         }
         self.allowedSections = ["general", "malware", "url_list", "reputation"]
+        self.cyclicbuff = []
+        self.automarshall = automarshall
+        self.callback = callback
+        self.storeage = []
+        self.store = store
+        self.calldepth = calldepth
 
+    def get_cyclicBuff(self):
+        return self.cyclicbuff
     
+    def get_storage(self):
+        return self.storeage
+    
+    def get_maxdepth(self):
+        return self.calldepth
+    
+    def __del__(self):
+        if len(self.cyclicbuff) > 0 and self.automarshall:
+            if self.callback is not None:
+                self.callback(self)
+            else:
+                print("no callback exiting without automarshalling done...", file=sys.stderr)
+    
+
     def test(self):
         result = req.get("https://otx.alienvault.com/api/v1/users/me", headers=self.headers).status_code
         if result==200: return 0
         else: return result
 
     def get_by_field(self, ip, section):
+        if section not in self.allowedSections: 
+            print("Section selection not allowed! - please select from the available", file=sys.stderr)
+            return -1
         requestURL = "https://otx.alienvault.com/api/v1/indicators/IPv4/{ip}/{section}".format(ip=ip, section=section)
+        fieldData = self.gen_call(requestURL)
+        if self.store:
+            self.storeage.append(fieldData)
+        else:
+            return fieldData 
+
+    def gen_call(self, url):
         try:
-            request = req.get(requestURL, headers=self.headers)
+            request = req.get(url, headers=self.headers)
             if request.status_code >= 200 and request.status_code < 300:
                 return json.loads(request.content.decode('utf8'))
-            else: print(request.status_code)
+            elif request.status_code >= 500:
+                newReq = cycReq(url)
+                inB = inBuff(newReq, self.cyclicbuff)
+                if inB == False:
+                    self.cyclicbuff.append(newReq)
+                else:
+                    inB.inc()
+            else:
+                return -1
         except:
-            print("error!")
+            print("major request error!", file=sys.stderr)
+            return -1
+    
+    def fin(self):
+        buff = self.cyclicbuff
+        for v in buff:
+            while v is not None:
+                if v.get_count() >= self.calldepth:
+                    v = None
+                    continue
+                else:  
+                    self.gen_call(v.get_url())
+                    v.inc()
+        return self.storeage
+
+class cycReq:
+    def __init__(self, url):
+        self.url = url
+        self.count = 0
+    
+    def inc(self):
+        self.count += 1
+    
+    def get_count(self):
+        return self.count
+
+    def get_url(self):
+        return self.url
+
+# idk how efficent this method is. Feels like a better datastructure would enable faster searching but ideally this buff doesnt get too big
+def inBuff(cyc, buff):
+    url = cyc.get_url()
+    for c in buff:
+        if c.get_url() == url:
+            return c
+    return False
+
+def automagic_key():
+    if 'apikey.txt' in os.listdir():
+        with open('apikey.txt', 'r') as f:
+            apiKey = f.readlines()
+        apiKey = apiKey[0]
+    else:
+        try:
+            apiKey = os.environ['OTXKEY']
+        except:
+            print('Key doesnt exist in env var OTXKEY or on disk... address and re-run')
+            return False
+    return apiKey
+
--- otxlookup/otxlookup.py
+++ otxlookup/otxlookup.py
@@ -1,34 +1,25 @@
-import requests as req
 from tqdm import tqdm
-import os, ipaddress, json, time
+import ipaddress
 from lib.otx import otx
-
-def automagic_key():
-    if 'apikey.txt' in os.listdir():
-        with open('apikey.txt', 'r') as f:
-            apiKey = f.readlines()
-        apiKey = apiKey[0]
-    else:
-        try:
-            apiKey = os.environ['OTXKEY']
-        except:
-            print('Key doesnt exist in env var OTXKEY or on disk... address and re-run')
-            quit()
-    return apiKey
 
 
 if __name__ == "__main__":
     ip_list = [str(ip) for ip in ipaddress.IPv4Network("10.0.0.0/8")]
     results = [] 
-    otxrequester = otx(automagic_key())
-
+    otxrequester = otx()
+    ## Required Data ##
+    # Malware - if it exists and roughly what 
+    # HTTP scans - if available
+    # Pulses - for adversaries
+    #  
     ## current best: 32.51 secs for 100 requests -- means approx 11k requests an hour, greater than the 10k limit ## 
     ## TODO: we must go faster ##
     ## TODO: off load processing intelligently (e.g. not inline) ##
+    
     for ip in tqdm(ip_list[0:10]):
-        resp = otxrequester.get_by_field(ip, otxrequester.allowedSections[0])
-        results.append(resp['pulse_info']['count']) 
+        otxrequester.get_by_field(ip, otxrequester.allowedSections[0])
 
+    print(otxrequester.fin())
 
 
 
