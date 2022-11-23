3661ae2fede15e741604c34519b951de36890b16
fredflinch <fredflinch2@gmail.com>
2022-11-23 12:17:38 +1100

    updated some methods

--- otxlookup/lib/otx.py
+++ otxlookup/lib/otx.py
@@ -2,7 +2,8 @@ import requests as req
 import json, sys, os
 
 class otx:
-    def __init__(self, key=None, automarshall=False, callback=None, store=True, calldepth=3):
+    def __init__(self, key=None, automarshall=True, callback=None, 
+    store=True, calldepth=3, useragent="Nunya Louise Recardio"):
         if key is None: 
             self.key = automagic_key()
             if self.key == False: quit()
@@ -10,7 +11,7 @@ class otx:
             self.key = key
         self.headers = {
             'X-OTX-API-KEY': self.key,
-            'User-Agent': "Nunya Louise Recardio",
+            'User-Agent': useragent,
             'Content-Type': 'application/json'
         }
         self.allowedSections = ["general", "malware", "url_list", "reputation"]
@@ -20,15 +21,11 @@ class otx:
         self.storeage = []
         self.store = store
         self.calldepth = calldepth
-
-    def get_cyclicBuff(self):
-        return self.cyclicbuff
     
-    def get_storage(self):
-        return self.storeage
-    
-    def get_maxdepth(self):
-        return self.calldepth
+    def flush_storage(self):
+        del self.storeage
+        self.storeage = []
+        return True
     
     def __del__(self):
         if len(self.cyclicbuff) > 0 and self.automarshall:
@@ -36,7 +33,6 @@ class otx:
                 self.callback(self)
             else:
                 print("no callback exiting without automarshalling done...", file=sys.stderr)
-    
 
     def test(self):
         result = req.get("https://otx.alienvault.com/api/v1/users/me", headers=self.headers).status_code
@@ -54,6 +50,7 @@ class otx:
         else:
             return fieldData 
 
+    ## make a generic GET request with cyclic buff based problem storage ## 
     def gen_call(self, url):
         try:
             request = req.get(url, headers=self.headers)
@@ -71,17 +68,19 @@ class otx:
         except:
             print("major request error!", file=sys.stderr)
             return -1
-    
+
+    ## clears the cyclic buffer and returns the interal storage state -- doesnt make sense when store is false ## 
     def fin(self):
-        buff = self.cyclicbuff
-        for v in buff:
-            while v is not None:
-                if v.get_count() >= self.calldepth:
-                    v = None
-                    continue
-                else:  
-                    self.gen_call(v.get_url())
-                    v.inc()
+        if self.automarshall:
+            buff = self.cyclicbuff
+            for v in buff:
+                while v is not None:
+                    if v.get_count() >= self.calldepth:
+                        v = None
+                        continue
+                    else:  
+                        self.gen_call(v.get_url())
+                        v.inc()
         return self.storeage
 
 class cycReq:
--- otxlookup/otxlookup.py
+++ otxlookup/otxlookup.py
@@ -1,25 +1,26 @@
 from tqdm import tqdm
-import ipaddress
+import ipaddress, time
 from lib.otx import otx
 
+## Required Data ##
+# Malware - if it exists and roughly what 
+# HTTP scans - if available
+# Pulses - for adversaries
+def process(data):
+    print(data[0].keys())
 
 if __name__ == "__main__":
-    ip_list = [str(ip) for ip in ipaddress.IPv4Network("10.0.0.0/8")]
-    results = [] 
+    ip_list = [str(ip) for ip in ipaddress.IPv4Network("10.0.0.0/16")] 
     otxrequester = otx()
-    ## Required Data ##
-    # Malware - if it exists and roughly what 
-    # HTTP scans - if available
-    # Pulses - for adversaries
-    #  
+
     ## current best: 32.51 secs for 100 requests -- means approx 11k requests an hour, greater than the 10k limit ## 
     ## TODO: we must go faster ##
-    ## TODO: off load processing intelligently (e.g. not inline) ##
-    
-    for ip in tqdm(ip_list[0:10]):
+    ## TODO: off load processing intelligently (e.g. not inline) ## 
+    for ip in tqdm(ip_list[0:1]):
         otxrequester.get_by_field(ip, otxrequester.allowedSections[0])
 
-    print(otxrequester.fin())
+    process(otxrequester.fin())
+
 
 
 
